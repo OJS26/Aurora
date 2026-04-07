6f7c661a7115f95d09881333bee9b69610c9b014
OJS26 <oliverstrachan_01@hotmail.co.uk>
2026-04-07 09:36:59 +0100

    Initial Commit

---
+++ Aurora/AuroraApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuroraApp.swift
+//  Aurora
+//
+//  Created by OJ Strachan on 07/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct AuroraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
